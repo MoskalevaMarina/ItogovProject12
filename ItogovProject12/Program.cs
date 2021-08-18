081c67e2c9b5f73f00a7f338dd08a18527ab03af
MoskalevaMarina <mari10102008@yandex.ru>
2021-08-18 22:57:13 +0400

    Задание 12.1.5

---
+++ ItogovProject12/Program.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="Program.cs" company="My Company Marina">
+// Copyright (c) 2021 All Rights Reserved
+// </copyright>
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ItogovProject12
+{
+    /// <summary>
+    /// Класс Program
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// метод Main
+        /// </summary>
+        public static void Main()
+        {
+            List<User> spisok = new List<User> { new User("Mai", "Маша", true), new User("Sasha", "Саша", false), new User("Pit", "Петя", false) };
+
+            foreach (var item in spisok)
+            {
+                Console.WriteLine($"Здравствуйте, {item.Name}");
+                if (!item.IsPremium)
+                {
+                    ShowAds();
+                }
+            }
+        }
+
+        /// <summary>
+        /// метод вывода рекламы
+        /// </summary>
+        public static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ ItogovProject12/User.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="User.cs" company="My Company Marina">
+// Copyright (c) 2021 All Rights Reserved
+// </copyright>
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItogovProject12
+{
+    /// <summary>
+    /// Класс Пользователь
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class.
+        /// </summary>
+        /// <param name="login">Ввод логина пользоателя</param>
+        /// <param name="name">Ввод имени пользователя</param>
+        /// <param name="isPremium">Ввод наличия премиум-подписки пользоателя</param>
+        public User(string login, string name, bool isPremium)
+        {
+            this.Login = login;
+            this.Name = name;
+            this.IsPremium = isPremium;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether поле для хранения логина
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether поле для хранения имени
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether поле для хранения наличия премиум-подписки
+        /// </summary>
+        public bool IsPremium { get; set; }
+    }
+}
